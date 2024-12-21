8aa2d85215bdb16e5e5032737838fd6fc4c51b73
zumavla <marinazubialevich@gmail.com>
2024-12-21 17:49:09 +0100

    Added EndGame and Resign functionality

--- app/src/main/java/com/example/cross_zero/MainActivity.java
+++ app/src/main/java/com/example/cross_zero/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cross_zero;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,17 +16,73 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import android.util.Log;
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import android.content.DialogInterface;
+import androidx.appcompat.app.AlertDialog;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import android.os.Handler;
 
 
 public class MainActivity extends AppCompatActivity {
+    ConstraintLayout rootLayout;
     String mySign;
-    Boolean Play = false;
+    Boolean Play;
     Button[][] buttons = new Button[3][3];
 
     String[][] board = new String[3][3];
 
     private MqttClient mqttClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        rootLayout = findViewById(R.id.main);
+        ViewCompat.setOnApplyWindowInsetsListener(rootLayout, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Populate the array with Button references
+        buttons[0][0] = findViewById(R.id.button00);
+        buttons[1][0] = findViewById(R.id.button10);
+        buttons[2][0] = findViewById(R.id.button20);
+        buttons[0][1] = findViewById(R.id.button01);
+        buttons[1][1] = findViewById(R.id.button11);
+        buttons[2][1] = findViewById(R.id.button21);
+        buttons[0][2] = findViewById(R.id.button02);
+        buttons[1][2] = findViewById(R.id.button12);
+        buttons[2][2] = findViewById(R.id.button22);
+        controlButtons(false);
+        showButtons(false);
+
+    }
+
+
+    private void showYesNoResign() {        // Create an AlertDialog for resigning
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        // Set the title and message for the dialog
+        builder.setTitle("Resignation confirmation:")
+            .setMessage("Do you want to resign?")
+
+            .setPositiveButton("Resign", (dialog, which) -> {
+                Toast.makeText(MainActivity.this, "You resigned", Toast.LENGTH_SHORT).show();
+                disconnectFromMqttBroker();
+                controlButtons(false);
+                showButtons(false);
+                Button button = findViewById(R.id.buttonStart);
+                button.setText(R.string.start);
+            })
+            .setNegativeButton("Continue playing", (dialog, which) -> Toast.makeText(MainActivity.this, "You chose to continue playing", Toast.LENGTH_SHORT).show());
+
+        // Create and show the dialog
+        AlertDialog resignDlg = builder.create();
+        resignDlg.show();
+    }
+
 
     public void controlButtons(Boolean state) {
         for (Button[] buttonz : buttons) {
@@ -58,15 +115,13 @@ public class MainActivity extends AppCompatActivity {
                 MqttConnectOptions options = new MqttConnectOptions();
                 options.setCleanSession(true);
                 mqttClient.connect(options);
-                Log.d("MQTT", "Reconnected to broker");
+                Log.d("MQTT", "(Re)connected to broker");
             } else {
                 Log.d("MQTT", "Already connected to the broker");
             }
 
             // Subscribe to a topic
-            mqttClient.subscribe("test/topic", 0, (topic, message) -> {
-                Log.d("MQTT", "Message received: " + new String(message.getPayload()));
-            });
+            mqttClient.subscribe("test/topic", 0, (topic, message) -> Log.d("MQTT", "Message received: " + new String(message.getPayload())));
         } catch (MqttException e) {
             e.printStackTrace();
             Log.e("MQTT", "Error while connecting or subscribing");
@@ -94,15 +149,94 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void initGame() {
+        Play = false;
+
+        if (Play) {
+            mySign = "X";
+        }
+        else {
+            mySign = "O";
+        }
+        board[0][0] = "";
+        board[1][0] = "";
+        board[2][0] = "";
+        board[0][1] = "";
+        board[1][1] = "";
+        board[2][1] = "";
+        board[0][2] = "";
+        board[1][2] = "";
+        board[2][2] = "";
+        redrawCells(buttons, board);
+
+        showButtons(true);
+
+        connectToMqttBroker();
+    }
 
     public void startGame(View view) {
-        showButtons(true);
-        connectToMqttBroker();
+        Button button = (Button) view;
+        if (button.getText().equals(getResources().getString(R.string.start))) {
+            initGame();
+            button.setText(R.string.resign);
+            button.setOnLongClickListener(this::resignGame);   // adding long tap functionality
+        }
+    }
+
+    public boolean initEndGame(View view) {
+        int[] cell1 = {0, 0};  // First cell coordinates
+        int[] cell2 = {1, 1};  // Second cell coordinates
+        int[] cell3 = {2, 2};  // Third cell coordinates
+
+        endGame("draw", cell1, cell2, cell3);
+        return true;
+    };
+    public void endGame(String outcome, int[] cell1, int[] cell2, int[] cell3) {
+        rootLayout.setEnabled(false);
+        new Handler().postDelayed(() -> {
+            rootLayout.setEnabled(true);
+        }, 3100);
+
+        if (outcome.equals("draw")) {
+            Toast.makeText(MainActivity.this, "It is a draw...", Toast.LENGTH_SHORT).show();
+            rootLayout.setEnabled(true);
+        }
+        else {
+            int defaultColor = ContextCompat.getColor(this, R.color.light_grey);
+            final int outcomeColor;
+            if (outcome.equals("victory")) {
+                outcomeColor = ContextCompat.getColor(this, R.color.olive_green);
+            } else {
+                outcomeColor = ContextCompat.getColor(this, R.color.red);
+            }
+            for (int i = 0; i < 3; i++ ) {
+                new Handler().postDelayed(() -> {
+                    buttons[cell1[0]][cell1[1]].setBackgroundTintList(ColorStateList.valueOf(outcomeColor));
+                    buttons[cell2[0]][cell2[1]].setBackgroundTintList(ColorStateList.valueOf(outcomeColor));
+                    buttons[cell3[0]][cell3[1]].setBackgroundTintList(ColorStateList.valueOf(outcomeColor));
+                }, 50 + 1000*i);
+                new Handler().postDelayed(() -> {
+                    buttons[cell1[0]][cell1[1]].setBackgroundTintList(ColorStateList.valueOf(defaultColor));
+                    buttons[cell2[0]][cell2[1]].setBackgroundTintList(ColorStateList.valueOf(defaultColor));
+                    buttons[cell3[0]][cell3[1]].setBackgroundTintList(ColorStateList.valueOf(defaultColor));
+                }, 550 + 1000*i);
+            }
+
+        }
+
+
+    }
+
+    public boolean resignGame(View view) {             // Returns boolean to handle long tap event
+
+        showYesNoResign();
+        return true; // Allow further processing
     }
 
     public void opponentTurn(View view) {
         Button button = (Button) view;
-        button.setText("Your turn!");
+        button.setText(R.string.your_turn);
+        button.setOnLongClickListener(this::initEndGame);
         redrawCells(buttons, board);
         controlButtons(true);  // Enable the button
 
@@ -114,7 +248,7 @@ public class MainActivity extends AppCompatActivity {
                 String cell = _board[i][j];
                 _buttons[i][j].setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.light_grey));
                 // Check if the cell is null
-                if (cell.equals("")) {
+                if (cell.isEmpty()) {
                     _buttons[i][j].setText("?");  // Display question mark if cell is null
                 }
                 else if (cell.equals("0")) {
@@ -127,46 +261,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        if (Play) {
-            mySign = "X";
-        }
-        else {
-            mySign = "O";
-        }
-        board[0][0] = "0";
-        board[1][0] = "";
-        board[2][0] = "";
-        board[0][1] = "0";
-        board[1][1] = "1";
-        board[2][1] = "";
-        board[0][2] = "1";
-        board[1][2] = "";
-        board[2][2] = "";
-
-        // Populate the array with Button references
-        buttons[0][0] = findViewById(R.id.button00);
-        buttons[1][0] = findViewById(R.id.button10);
-        buttons[2][0] = findViewById(R.id.button20);
-        buttons[0][1] = findViewById(R.id.button01);
-        buttons[1][1] = findViewById(R.id.button11);
-        buttons[2][1] = findViewById(R.id.button21);
-        buttons[0][2] = findViewById(R.id.button02);
-        buttons[1][2] = findViewById(R.id.button12);
-        buttons[2][2] = findViewById(R.id.button22);
-        controlButtons(false);
-        showButtons(false);
-
-    }
     public void onCellClick(View view) {
         // Cast the view to a Button
         Button button = (Button) view;
@@ -181,7 +275,7 @@ public class MainActivity extends AppCompatActivity {
             button.setText(mySign);
             controlButtons(false);
             Button button2 = findViewById(R.id.button); // Find the button by its ID
-            button2.setText("Your opponent's turn..."); // Update the button's text
+            button2.setText(R.string.your_opponent_s_turn); // Update the button's text
             String buttonIdName = getResources().getResourceEntryName(view.getId());
             int i = Character.getNumericValue(buttonIdName.charAt(6));      // x coord
             int j = Character.getNumericValue(buttonIdName.charAt(7));      // y coord
